43469e21bc34685872a289ff6fefdc86fa7540c0
EronVasconcelos <eronvasconcelos.br@gmail.com>
2025-12-26 23:50:44 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-self.addEventListener('fetch', function(event) {});
+const cacheName = 'precificecom-v1';
+const assets = ['./', './index.html', './logo-svg.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
